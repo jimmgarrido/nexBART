d8632a4fa8c20f878c1f378754174d3dcc438d30
Jimmy Garrido <jimmygarrido@outlook.com>
2017-06-08 18:19:00 -0700

    Begin XL UI

--- nexBart.UWP/Views/AlertsView.xaml.cs
+++ nexBart.UWP/Views/AlertsView.xaml.cs
@@ -1,27 +1,33 @@
-﻿using nexBart.ViewModels;
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
+
+using nexBart.ViewModels;
 
 namespace nexBart.UWP.Views
 {
 	public sealed partial class AlertsView : UserControl
 	{
 		AlertsModel ViewModel;
-		Task Initialization;
-
 
 		public AlertsView()
 		{
 			this.InitializeComponent();
 
 			ViewModel = new AlertsModel();
-			Initialization = InitializationAsync();
+			this.Loaded += AlertsView_Loaded;
+
+			//Initialization = InitializationAsync();
+		}
+
+		async void AlertsView_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			alertsGroup.Source = await ViewModel.RefreshAlerts();
 		}
 
 		async Task InitializationAsync()
 		{
 			alertsGroup.Source = await ViewModel.RefreshAlerts();
-		}
+		}		
 	}
 }
